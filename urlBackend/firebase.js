2616bfe07103a83712e21f0c700391d4ee9463b5
YogeshTiwari2000 <yogeshtiwari.b.007@gmail.com>
2025-04-01 16:09:22 +0530

    env setup

--- urlBackend/firebase.js
+++ urlBackend/firebase.js
@@ -1,13 +1,43 @@
+// import { initializeApp, cert } from 'firebase-admin/app';
+// import { getFirestore } from 'firebase-admin/firestore';
+// import serviceAccount from './firebase-service-account.json' assert { type: 'json' }; // Use import for JSON
+
+
+// initializeApp({
+//     credential: cert(serviceAccount)
+// });
+
+// const db = getFirestore();
+
+// export { db };
+import * as dotenv from 'dotenv';
+// dotenv.config();
+dotenv.config({ path: './environment.env' });
+
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import serviceAccount from './firebase-service-account.json' assert { type: 'json' }; // Use import for JSON
 
-// const serviceAccount = require('./firebase-service-account.json'); // Path to your service account key file
+
+
+console.log('Project ID:', process.env.FIREBASE_PROJECT_ID);
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: 'googleapis.com'
+};
 
 initializeApp({
-    credential: cert(serviceAccount)
+    credential: cert(serviceAccount),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 });
 
-const db = getFirestore();
-
-export { db };
+export const db = getFirestore();
