c431a09e3d15b158121029128f12c82382964614
YogeshTiwari2000 <yogeshtiwari.b.007@gmail.com>
2025-04-01 16:15:44 +0530

    Auto-generated commit

--- server/assets-chunks/styles-7Y5LZEVX_css.mjs
+++
@@ -1,2 +0,0 @@
-export default `*{margin:0;padding:0}:root{--tektur-font: "Tektur", sans-serif;--tektur-optical-sizing: auto;--tektur-weight: 400;--tektur-style: normal;--tektur-variation-wdth: 100;--montserrat-underline-regular: "Montserrat Underline", sans-serif;--montserrat-underline-font-family: "Montserrat Underline", sans-serif;--noto-sans-gunjala-gondi-regular: "Noto Sans Gunjala Gondi", sans-serif}
-`;
